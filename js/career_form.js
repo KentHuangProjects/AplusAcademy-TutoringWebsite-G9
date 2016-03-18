d27cee9daf46e6ea7017bfce6c2756bceb229b7d
Kent Huang <kenttttt0@gmail.com>
2016-03-18 15:32:31 -0700

    hello world

---
+++ js/career_form.js
@@ -0,0 +1,154 @@
+function $$(id){
+	var element = document.getElementById(id);
+	if( element == null ) {
+		//alert( "Programmer error: " + id + " does not exist." );
+	}
+	return element;
+}
+
+/*---------- Validate entire form ----------*/
+function validateForm() {
+    // Warn all invalid fields
+    warnNameInvalid('txtFirstName');
+    warnNameInvalid('txtLastName');
+    warnEmailInvalid('txtEmail');
+    warnPhoneInvalid('txtPhone');
+    warnAddressInvalid('txtAddress');
+    warnCityInvalid('selCity');
+    warnCoverLetterInvalid('filCoverLetter');
+    warnResumeInvalid('filResume');
+    warnAboutInvalid('taAbout');
+
+    // Check all fields
+    if(!testNameValid('txtFirstName')) {
+        return false;
+    } else if(!testNameValid('txtLastName')) {
+        return false;
+    } else if(!testEmailValid('txtEmail')) {
+        return false;
+    } else if(!testPhoneValid('txtPhone')) {
+        return false;
+    } else if(!testAddressValid('txtAddress')) {
+        return false;
+    } else if(!testCityValid('selCity')) {
+        return false;
+    } else if(!testCoverLetterValid('filCoverLetter')) {
+        return false;
+    } else if(!testResumeValid('filResume')) {
+        return false;
+    } else if(!testAboutValid('taAbout')) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*---------- Highlights field if invalid ----------*/	
+function warn(id, valid) {
+	if(valid) {
+	    $$(id).style.border = "none";
+	}
+    else {  
+        $$(id).style.border = "2px solid red";
+    }
+}
+
+/*---------- Name validation ----------*/
+function testNameValid(id) {
+	return($$(id).value.length > 0);
+}
+			
+function warnNameInvalid(id) {
+    var valid = testNameValid(id);
+	warn(id, valid);
+}
+
+/*---------- Email validation ----------*/
+function testEmailValid(id) {
+	var email = $$(id).value;
+
+    // Email should have a "." and a "@"
+	return (email.search(/\S+@\S+\.\S+/) == 0);
+}
+			
+function warnEmailInvalid(id) {
+    warn(id, testEmailValid(id));
+}
+
+/*---------- Phone validation ----------*/
+function testPhoneValid(id) {
+    var phoneStr = $$(id).value;
+
+    // Phone format should be: 123-456-7890 or 123 456 7890
+    var match = phoneStr.search(/[0-9]{3}[- ][0-9]{3}[- ][0-9]{4}/);
+
+	return(match == 0);
+}
+			
+function warnPhoneInvalid(id) {
+    warn(id, testPhoneValid(id));
+}
+
+// Separates phone number with dashes as user types
+function formatPhone(id, event) {
+    var str = $$(id).value;
+    var length = str.length;
+
+    // Check if backspace is pressed
+    if(event.keyCode == 8){
+        if(length == 4 || length == 8) {
+            $$(id).value = str.substring(0, length - 1); // Remove dash
+        }
+    } else if(length == 3 || length == 7) {
+        $$(id).value += "-"; // Add dash
+    }
+}
+
+/*---------- Address validation ----------*/
+function testAddressValid(id) {
+    return ($$(id).value.length > 0);
+}
+			
+function warnAddressInvalid(id) {
+    warn(id, testAddressValid(id));
+}
+
+/*---------- City validation ----------*/
+function testCityValid(id) {
+    return ($$(id).selectedIndex != 0);
+}
+			
+function warnCityInvalid(id) {
+    warn(id, testCityValid(id));
+}
+
+/*---------- Cover Letter validation ----------*/
+function testCoverLetterValid(id) {
+    var fileName = $$(id).value;
+
+    return (fileName.endsWith(".pdf") || fileName.endsWith(".doc") || fileName.endsWith(".docx"));
+}
+			
+function warnCoverLetterInvalid(id) {
+    warn(id, testCoverLetterValid(id));
+}
+
+/*---------- Resume validation ----------*/
+function testResumeValid(id) {
+    var fileName = $$(id).value;
+
+    return (fileName.endsWith(".pdf") || fileName.endsWith(".doc") || fileName.endsWith(".docx"));
+}
+			
+function warnResumeInvalid(id) {
+    warn(id, testResumeValid(id));
+}
+
+/*---------- About validation ----------*/
+function testAboutValid(id) {
+    return($$(id).value.length > 0);
+}
+			
+function warnAboutInvalid(id) {
+    warn(id, testAboutValid(id));
+}
---
+++ js/registeration_Form_script.js
@@ -0,0 +1,60 @@
+			/*Last name and first name's validation.*/ 
+			$(document).ready(function(){
+				$("#regForm").submit(function(e){
+					var lastName = $("#lastName").val();
+					var firstName = $("#firstName").val();
+					if(firstName.length == 0 || lastName.length == 0){
+						$("#hintNameValid").text("Please fill up your name.");
+						e.preventDefault();
+						} else {
+						$("#hintNameValid").text("");
+						}
+					});
+				});
+				/*Gender selection validation.*/ 
+				$(document).ready(function(){
+				$("#regForm").submit(function(e){
+					if($('input[type=radio]:checked').length == 0){
+						$("#hintGenderValid").text("Please select your gender.");
+						e.preventDefault();
+						} else {
+						$("#hintGenderValid").text("");
+						}
+					});
+				});
+				/*Email validation.*/ 
+				$(document).ready(function(){
+				$("#regForm").submit(function(e){
+				var reg = /\S+@\S+\.\S+/;
+					if(!reg.test($("#email").val())){
+						$("#hintEmailValid").text("Please enter a valid Email Address.");
+						e.preventDefault();
+						} else {
+						$("#hintEmailValid").text("");
+						}
+					});
+				});
+				/*Password validation.*/ 
+				$(document).ready(function(){
+				$("#regForm").submit(function(e){
+				var reg1 = /[a-zA-Z0-9]{7,}/;
+				var reg2 = /.*[a-z].*/;
+				var reg3 = /.*[A-Z].*/;
+				var reg4 = /.*[0-9].*/;
+				var format = Boolean(reg1.test($("#password_0").val()) && reg2.test($("#password_0").val()) && reg3.test($("#password_0").val()) && reg4.test($("#password_0").val()));
+					if(!format){
+						$(".hintPasswordValid").css("color", "red");
+						$(".hintPasswordValid").text("Must contain at least 7 characters; at least 1 lowercase, 1 uppercase and 1 number.");
+						e.preventDefault();
+						}
+						else if($("#password_0").val() != $("#password_1").val()){
+						$(".hintPasswordValid").css("color", "red");
+						$(".hintPasswordValid").text("Two password are different.");
+						} else {
+						$(".hintPasswordValid").css("color", "#b3abb9");
+						$(".hintPasswordValid").text("Must contain at least 7 characters; at least 1 lowercase, 1 uppercase and 1 number.");
+						}
+					});
+				});
+				
+				
