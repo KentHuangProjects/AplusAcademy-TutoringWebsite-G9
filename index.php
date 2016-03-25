8021f51ce949ec3ecb44f528e96f09d7293d72c2
Kabir Cahill <kabir_cahill@hotmail.com>
2016-03-25 15:20:09 -0700

    Added images to home page.

--- index.php
+++ index.php
@@ -49,9 +49,7 @@
                     </div>
                 </div>
 
-                <div class="content-block" id="economics">
-                    <h2>Economics</h2>
-
+                <div class="content-block">
                     <div class="col-1">
                           <h3>Courses</h3>
                           <ul>
@@ -62,6 +60,7 @@
                     </div>
 
                     <div class="col-2">
+                        <h2>Economics</h2>
                   	    <div class="description">
                             <p>Economics is the study of how people choose to use resources.</p>
                             <p>Resources include the basic four factors of production: Natural resources, Human resources, Capital and Entrepreneurship. And the knowledge of how to combine them to create useful products and services is the main idea in sucess.economics includes the study of labor, land, and investments, of money, income, and production, and of taxes and government expenditures. Economists seek to measure well-being, to learn how well-being may increase over time, and to evaluate the well-being of the rich and the poor.</p>
@@ -70,13 +69,11 @@
                     </div>
 
                     <div class="col-3">
-                        <img src="images/image.jpg" alt="Image" height="100" width="100">
+                        <a href="/subjects/subject_economy.html"><img src="/images/economics_title.jpg" alt="Economics Image" height="150" width="150"></a> 
                     </div>
                 </div>
 
                 <div class="content-block">
-                    <h2>Professional Writing</h2>
-
                     <div class="col-1">
                         <h3>Courses</h3>
                         <ul>
@@ -87,6 +84,7 @@
                     </div>
 
                     <div class="col-2">
+                        <h2>Professional Writing</h2>
                   	    <div class="description">
                               <p>Have you ever felt headache when it comes to writing for school or for work? Wondering why some people can write and communicate so concise and effectively? A+ Academy can help!</p>
                               <p>One of the most effective ways to communicate is through writing. We are currently offering essay, professional report and academy thesis writing courses. If you want to improve your communication skills in those areas, register with A+ Academy today!</p>
@@ -94,13 +92,11 @@
                     </div>
 
                     <div class="col-3">
-                        <img src="images/image.jpg" alt="Image" height="100" width="100">
+                        <a href="/subjects/subject_writing.php"><img src="/images/courses_Writing.jpg" alt="Writing Image" height="150" width="150"></a>
                     </div>
                 </div>
 
                 <div class="content-block">
-                    <h2>Computer Science</h2>
-                   
                     <div class="col-1">
                         <h3>Courses</h3>
                         <ul>
@@ -111,6 +107,7 @@
                     </div>
 
                     <div class="col-2">
+                        <h2>Computer Science</h2>
                         <div class="description">
                             <p>Aplus Academy offers beginner to intermediate level courses in computer science.</p>
                             <p>We currently teach three programming languages: HTML, C++ and Java. Whether you're just starting out or you already have some knowledge in computing, we have a course for you.</p>
@@ -118,13 +115,11 @@
                     </div>
 
                     <div class="col-3">
-                        <img src="images/image.jpg" alt="Image" height="100" width="100">
+                        <a href="/subjects/computer_science.php"><img src="/images/subject_computing.jpg" alt="Computing Image" height="150" width="150"></a>
                     </div>
                </div>
 
                 <div class="content-block">
-                    <h2>Math</h2>
-    
                     <div class="col-1">
                         <h3>Courses</h3>
                         <ul>
@@ -135,6 +130,7 @@
                     </div>
 
                     <div class="col-2">
+                        <h2>Math</h2>
                         <div class="description">
                             <p>Working on math homework now? We can help. Our math tutors are experts at Algebra, Calculus, Matirx, and everything in between.They’ll help with everything math class throws at you.</p>
                             <p>Whether you're still learning the basics or mastering more difficult concepts, we're ready to help. Sign up now and it is easy to Boost Your Math Grade.</p>
@@ -142,13 +138,11 @@
                     </div>
 
                     <div class="col-3">
-                        <img src="images/image.jpg" alt="Image" height="100" width="100">
+                        <a href="/subjects/subject_math.html"><img src="/images/subject_math.jpg" alt="Math Image" height="150" width="150"></a>
                     </div>
                 </div>
 
                 <div class="content-block">
-                    <h2>Fitness</h2>
-             
                     <div class="col-1">
                         <h3>Courses</h3>
                         <ul>
@@ -159,6 +153,7 @@
                     </div>
 
                     <div class="col-2">
+                        <h2>Fitness</h2>
                         <div class="description">
                             <p>Here at A+ Academy, not only do we look to improve your mental strength, but also your physical strength. With options like sports, weight training and dance you have a variety of ways to get the healthy physique you deserve.</p>
                             <p>Through the internet we can help you achieve your fitness goals at a time suitable to your schedule. We’re dedicated to developing the most effective training programs just for you and we strive to help you become the fittest you can be!</p>
@@ -166,7 +161,7 @@
                     </div>
 
                     <div class="col-3">
-                        <img src="images/image.jpg" alt="Image" height="100" width="100">
+                        <a href="/subjects/subject_fitness.html"><img src="/images/subject_fitness.jpg" alt="Fitness Image" height="150" width="150"></a>
                     </div>
                 </div>
             </div>
