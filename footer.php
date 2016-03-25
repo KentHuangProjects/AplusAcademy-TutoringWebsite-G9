40a50a33eb5e9b8a8cde84df1af09ce853b10d6d
Kabir Cahill <kabir_cahill@hotmail.com>
2016-03-25 12:00:24 -0700

    Updated footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
             <!-- Main Footer -->
             <div id="main-footer">
-		        <p>Copyright&copy;  |  Aplus .Co &nbsp;|&nbsp;  BCIT &nbsp;|&nbsp;  123-456-6789 &nbsp;|  Contact Us</p>
+		        <p>&copy; <?php echo date('Y'); ?> Aplus Academy &nbsp;|&nbsp; BCIT &nbsp;|&nbsp; 123-456-6789 &nbsp;|&nbsp; <a href="/contact_us.html">Contact Us</a></p>
 	        </div>
