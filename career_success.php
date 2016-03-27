35fc44bda229a8a68ea0c22cd8ba254051224bf8
Luke Lee <luke.lee7575@gmail.com>
2016-03-27 13:15:43 -0700

    Added updated career_bg.jpg

---
+++ career_success.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Career</title>
+
+    <script type="text/javascript" src="js/career_form.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!--CSS for registeration form-->
+    <link href="style/style_registration.css" rel="stylesheet" type="text/css"/>
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- Registeration form validation -->
+    <script type="text/javascript" src="js/registeration_Form_script.js"></script>
+
+    <link href="style/base.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style/style_career.css">
+</head>
+	
+<body>
+    <!-- main wrapper wraps the whole page -->
+    <div id="main-wrapper">
+
+        <?php
+            include ("header.php");
+        ?>
+
+        <!-- main content section -->
+	    <div id="main-content">
+		    <div id="header">
+			    <h1>Career</h1>
+		    </div>
+	        
+			<!-- Successful message -->
+			<div id="message">
+				<h3>Application Successful!!</h3>
+				<p>Thank you for applying with us. Please wait up to 2 weeks to process your application, and we will get back to you.</p>
+			</div>
+
+	    </div>
+
+        <!-- Main Footer -->
+        <div id="main-footer">
+		    <p>Copyright&copy;  |  Aplus .Co &nbsp;|&nbsp;  BCIT &nbsp;|&nbsp;  123-456-6789 &nbsp;|  Contact Us</p>
+	    </div>
+    </div>
+</body>
+</html>
