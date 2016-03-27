02d039144369a6a4f650725579898edd49c06075
rogerzhang18 <rogeryj.zhang@gmail.com>
2016-03-27 15:25:10 -0700

    Added files via upload

---
+++ subjects/subject_math.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link href="style/base.css" rel="stylesheet" type="text/css">
+        <link href="style/style_subject.css" rel="stylesheet" type="text/css">
+        <!--CSS for registeration form-->
+        <link href="style/style_registration.css" rel="stylesheet" type="text/css"/>
+
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <!-- Registeration form validation -->
+        <script type="text/javascript" src="js/registeration_Form_script.js"></script>
+<title>Math</title>
+<meta charset="utf-8">
+
+
+</head>
+
+    <body>
+        <div id="main-wrapper">
+        
+<?php
+	include ("../header.php");
+?>
+
+	    <header>
+	        <h1>Math</h1>
+	    </header>
+
+	    <div class="content">
+		
+		    <div class ="row1">
+			    <div id ="imag">
+				    <img src="images/subject_math.jpg" alt="Subject name" width="400" height="200"/>
+			    </div>
+			
+			    <div id ="intro">
+			    <h2>A place to boost your math grade!</h2>
+			    <p><br>Working on math homework now? We can help. Our math tutors are experts at Algebra, Calculus, Matirx, and everything in between.They’ll help with everything math class throws at you. <br><br>Whether you're still learning the basics or mastering more difficult concepts, we're ready to help. Sign up now and it is easy to Boost Your Math Grade.<br><br></p>
+			    
+			    </div>
+		    </div>
+
+		    <div class="row2">
+			
+			    <a href="courses_algebra.html">
+			    <div id ="c1" class="courses">
+			    <h3 id="c1title">Algebra</h3>
+			    <p>Algebra is the language through which we describe patterns. As opposed to having to do something over and over again, algebra gives you a simple way to express that repetitive process. It's also seen as a "gatekeeper" subject.</p>
+			    </div>
+			    </a>
+			
+			    <a href="courses_calculus.html">
+			    <div id ="c2" class="courses">
+			    <h3 id="c2title">Calculus</h3>
+			    <p>Calculus is a part of modern mathematics education. A course in calculus is a gateway to other, more advanced courses in mathematics devoted to the study of functions and limits, broadly called mathematical analysis.</p>
+			    </div>
+			    </a>
+			
+			    <a href="courses_matrix.html">
+			    <div id ="c3" class="courses">
+			    <h3 id="c3title">Matrix</h3>
+			    <p>Matrix, a major branch of numerical analysis, is devoted to the development of efficient algorithms for matrix computations, a subject that is centuries old and is today an expanding area of research.</p>
+			    </div>
+			    </a>
+		    </div>
+	    </div>
+<?php
+	include ("../footer.php");
+?>
+
+    </div>
+</body>
+</html>
