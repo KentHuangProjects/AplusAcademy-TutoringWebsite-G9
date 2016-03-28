1bd44827f89f291a1efd6feb49869a2bdcbd65c6
Luke Lee <luke.lee7575@gmail.com>
2016-03-28 00:13:27 -0700

    Updated footer

--- career_unsuccess.php
+++ career_unsuccess.php
@@ -55,9 +55,9 @@
 	    </div>
 
         <!-- Main Footer -->
-        <div id="main-footer">
-		    <p>Copyright&copy;  |  Aplus .Co &nbsp;|&nbsp;  BCIT &nbsp;|&nbsp;  123-456-6789 &nbsp;|  Contact Us</p>
-	    </div>
+        <?php
+            include("../footer.php");
+        ?>
     </div>
 </body>
 </html>
